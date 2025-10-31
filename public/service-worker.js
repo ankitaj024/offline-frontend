bfc01802eef8c43ebe10de9dc5da896031b049cc
vipulkmit <vipul.kmit@gmail.com>
2025-10-31 17:39:14 +0530

    fix:service worker api hitting one

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,5 +1,5 @@
 const CACHE_NAME = 'offline-cache-v4';
-const API_BASE = 'http://localhost:4000/api';
+const API_BASE = 'https://fd3de600e774.ngrok-free.app/api';
 const ASSETS = [
   '/',
   '/form',
